7a81e27b54d5204b84ad0e7ecc95c39b2aa34524
DEV1 <Suresh@DESKTOP-BMH0DNA>
2025-02-07 16:48:38 +0530

    project modify by dev1

--- dnyanu/src/com/nt/sbean/sample.java
+++ dnyanu/src/com/nt/sbean/sample.java
@@ -6,8 +6,13 @@ public class sample {
 	{
 		System.out.println("hello");
 	}
+	void meth2()
+	{
+		System.out.println("Hi Navya");
+	}
 	public static void main(String[] args) {
 		sample sam=new sample();
+		sam.meth1();
 		sam.meth1();
 		
 
