33e4c2775520828407672baa0fb47a843c662e56
DEV1 <Suresh@DESKTOP-BMH0DNA>
2025-02-07 16:42:28 +0530

    project initial by TL

---
+++ dnyanu/src/com/nt/sbean/sample.java
@@ -0,0 +1,16 @@
+package com.nt.sbean;
+
+public class sample {
+
+	void meth1()
+	{
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		sample sam=new sample();
+		sam.meth1();
+		
+
+	}
+
+}
