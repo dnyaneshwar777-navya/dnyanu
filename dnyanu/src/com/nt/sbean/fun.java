2abf7ca698dcdd279999ccd8e6f42dc422fb6017
DEV1 <Suresh@DESKTOP-BMH0DNA>
2025-02-07 18:13:39 +0530

    suresh tl

---
+++ dnyanu/src/com/nt/sbean/fun.java
@@ -0,0 +1,12 @@
+package com.nt.sbean;
+
+public class fun {
+ void fuk() {
+	 System.out.println("fun.fuk()");
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
